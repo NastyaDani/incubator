0be37659dc28121c18bd573f1066154d88365064
Neto test <anastasiya.parfinenko@gmail.com>
2024-01-29 13:00:18 +0600

    task.js

--- task.js
+++ task.js
@@ -1,3 +1,5 @@
+
+//Количество гласных
 function getCount(str) {
     let sum = 0;
     let vs = "aeiou";
@@ -10,7 +12,7 @@ function getCount(str) {
     }, 0);
     return r;
 }
-
+// Классифицировать нового участника
 function openOrSenior(data){
     let result = data.map(function(member){
       if (member[0]>=55 && member[1]>7){
@@ -20,3 +22,10 @@ function openOrSenior(data){
     })
       return result;
     }
+// Сумма двух наименьших положительных целых чисел
+
+    function sumTwoSmallestNumbers(numbers) {
+        let arr = numbers.sort((a,b)=> a-b);
+        let result = arr[0]+arr[1];
+        return result;
+      }
