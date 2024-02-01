270ac34cc5f2ad3fd67c8102595104bf42adbf41
Neto test <anastasiya.parfinenko@gmail.com>
2024-02-01 17:01:01 +0600

    task.js

--- task.js
+++ task.js
@@ -104,3 +104,11 @@ function longest(s1, s2) {
     return set;
 }
 
+//A Needle in the Haystack
+function findNeedle(haystack) {
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === "needle") {
+            return "found the needle at position " + haystack.indexOf(haystack[i]);
+        }
+    }
+}
